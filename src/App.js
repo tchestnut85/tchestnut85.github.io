32acd2d959c76c386507ee53cec5ea2275a508e9
tchestnut85 <tchestnut85@gmail.com>
2020-12-30 22:44:05 -0500

    added a footer component with links to my github, linkedin etc and added css styling. also installed fontawesome for react to use icons for links.

--- src/App.js
+++ src/App.js
@@ -2,14 +2,17 @@ import React, { useState } from 'react';
 import './index.css';
 import Header from './components/Header';
 import About from './components/About';
+import Footer from './components/Footer';
 
 function App() {
+
   return (
     <div>
       <Header></Header>
       <main>
         <About></About>
       </main>
+      <Footer></Footer>
     </div>
   );
 }
---
+++ src/components/Icon/index.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLinkedin, faGithubSquare, faHackerrank, faStackOverflow } from '@fortawesome/free-brands-svg-icons';
+
+function Icon(props) {
+
+    const { links } = props;
+    console.log(links);
+
+    function iconName(id) {
+        switch (id) {
+            case 'link-1':
+                return faLinkedin;
+            case 'link-2':
+                return faGithubSquare;
+            case 'link-3':
+                return faHackerrank;
+            case 'link-4':
+                return faStackOverflow;
+        }
+    }
+
+    return (
+        <div className='flex space-b wrap'>
+            {links.map(link => (
+                <span>
+                    <a href={link.url} target='_blank' className='icon-link'>
+                        <FontAwesomeIcon
+                            className='icons'
+                            key={link.id}
+                            icon={iconName(link.id)}
+                        />
+                        <p className='link-name'>
+                            {link.name}
+                        </p>
+                    </a>
+                </span>
+            ))}
+        </div>
+    );
+}
+
+export default Icon;
