db6ca217bfa68d2b439df9fb95bc16db7f1e30f8
tchestnut85 <tchestnut85@gmail.com>
2021-01-10 17:17:57 -0500

    split up front-end and back-end skills in the resume section.

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -4,78 +4,98 @@ function Resume() {
     const skills = [
         {
             name: 'Javascript',
-            file: 'javascript.svg'
-        },
-        {
-            name: 'CSS',
-            file: 'css.svg'
+            file: 'javascript.svg',
+            type: 'front-end'
         },
         {
             name: 'HTML',
-            file: 'html-5.svg'
+            file: 'html-5.svg',
+            type: 'front-end'
+        },
+        {
+            name: 'CSS',
+            file: 'css.svg',
+            type: 'front-end'
         },
         {
             name: 'jQuery',
-            file: 'jquery.svg'
+            file: 'jquery.svg',
+            type: 'front-end'
         },
         {
             name: 'React',
-
-            file: 'react.svg'
+            file: 'react.svg',
+            type: 'front-end'
         },
         {
             name: 'Node',
-            file: 'nodejs.svg'
+            file: 'nodejs.svg',
+            type: 'back-end'
         },
         {
             name: 'Express',
-            file: 'expressjs.svg'
+            file: 'expressjs.svg',
+            type: 'back-end'
         },
         {
             name: 'Handlebars',
-            file: 'handlebars.svg'
+            file: 'handlebars.svg',
+            type: 'front-end'
         },
         {
             name: 'Bootstrap',
-            file: 'bootstrap.svg'
+            file: 'bootstrap.svg',
+            type: 'front-end'
         },
         {
             name: 'Materialize',
-            file: 'materializecss.svg'
+            file: 'materializecss.svg',
+            type: 'front-end'
         },
         {
             name: 'Bulma',
-            file: 'bulma.svg'
+            file: 'bulma.svg',
+            type: 'front-end'
         },
         {
             name: 'MySQL',
-            file: 'mysql.svg'
+            file: 'mysql.svg',
+            type: 'back-end'
         },
         {
             name: 'Sequelize',
-            file: 'sequelize.svg'
+            file: 'sequelize.svg',
+            type: 'back-end'
         },
         {
             name: 'MongoDB',
-            file: 'mongodb.svg'
+            file: 'mongodb.svg',
+            type: 'back-end'
         },
         {
             name: 'Mongoose',
-            file: 'mongoose.png'
+            file: 'mongoose.png',
+            type: 'back-end'
         },
         {
             name: 'Jest',
-            file: 'jest.svg'
+            file: 'jest.svg',
+            type: 'back-end'
         },
         {
             name: 'Git',
-            file: 'git-icon.svg'
+            file: 'git-icon.svg',
+            type: 'back-end'
         },
         {
             name: 'Webpack',
-            file: 'webpack.svg'
+            file: 'webpack.svg',
+            type: 'back-end'
         }
     ];
+
+    const frontends = skills.filter(skill => skill.type === 'front-end');
+    const backends = skills.filter(skill => skill.type === 'back-end');
 
     return (
         <section className='resume-section'>
@@ -85,9 +105,18 @@ function Resume() {
             <p className='content resume-text'>View or download my full resume
                 <a className='resume-link' href='https://drive.google.com/file/d/1M4PNYrAi1U5XrBmbfFSP-DXe4pLXNSMs/view?usp=sharing' target='_blank' rel='noopener noreferrer'>here.</a>
             </p>
-            <h3 className='heading'>My Skills</h3>
+            <h3 className='heading'>Front-End Skills</h3>
             <div className='icon-list'>
-                {skills.map((skill) => (
+                {frontends.map((skill) => (
+                    <div key={skill.name} className='skill-div flex space-b'>
+                        <img src={require(`../../assets/skill-icons/${skill.file}`).default} className='skill-icon' alt={`Icon for ${skill.name}`} />
+                        <span className='skill-text'>{skill.name}</span>
+                    </div>
+                ))}
+            </div>
+            <h3 className='heading'>Back-End Skills</h3>
+            <div className='icon-list'>
+                {backends.map((skill) => (
                     <div key={skill.name} className='skill-div flex space-b'>
                         <img src={require(`../../assets/skill-icons/${skill.file}`).default} className='skill-icon' alt={`Icon for ${skill.name}`} />
                         <span className='skill-text'>{skill.name}</span>
