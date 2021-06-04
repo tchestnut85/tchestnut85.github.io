4488b245df6f42a5c498161851b64d0a1ba067db
Tom Chestnut <tchestnut85@gmail.com>
2021-06-04 09:16:06 -0400

    Fixed white space below header bug, tweaked some CSS, and updated the copy email function in the Contacts component to have a setTimeout so the "email copied" confirm message dissapears after a few seconds.

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -4,67 +4,93 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCopy } from '@fortawesome/free-regular-svg-icons';
 
 function Contact() {
+	const [emailCopied, setEmailCopied] = useState('');
 
-    const [emailCopied, setEmailCopied] = useState('');
+	const copyEmail = () => {
+		const email = document.getElementById('email');
+		// const range = document.createRange();
+		// range.selectNodeContents(email);
+		// window.getSelection().addRange(range);
 
-    const copyEmail = () => {
-        const email = document.getElementById('email');
-        // const range = document.createRange();
-        // range.selectNodeContents(email);
-        // window.getSelection().addRange(range);
+		// try {
+		//     const success = document.execCommand('copy');
+		//     const message = success ? 'Email Copied!' : "Email wasn't copied";
+		//     console.log(message);
+		// } catch (err) {
+		//     console.error('Unable to copy email.', err);
+		// }
 
-        // try {
-        //     const success = document.execCommand('copy');
-        //     const message = success ? 'Email Copied!' : "Email wasn't copied";
-        //     console.log(message);
-        // } catch (err) {
-        //     console.error('Unable to copy email.', err);
-        // }
+		// window.getSelection().removeAllRanges();
 
-        // window.getSelection().removeAllRanges();
+		navigator.clipboard
+			.writeText(email.textContent)
+			.then(() => {
+				const message = 'Email Copied!';
+				console.log(message);
+			})
+			.catch(error => {
+				console.log('Email not copied:', error);
+			});
 
+		if (email) {
+			setEmailCopied('Email Copied!');
+			setTimeout(() => {
+				setEmailCopied('');
+			}, 3000);
+		} else {
+			setEmailCopied('');
+		}
+	};
 
-        navigator.clipboard.writeText(email.textContent).then(() => {
-            const message = 'Email Copied!';
-            console.log(message);
-        }).catch(error => {
-            console.log('Email not copied:', error);
-        });
-
-        if (email) {
-            setEmailCopied('Email Copied!');
-        } else {
-            setEmailCopied('');
-        }
-    };
-
-    return (
-        <section className='contact-me-section'>
-            <h2 className='heading'>Contact Me</h2>
-            <div>
-                <div>
-                    <h3 className='heading'>Email</h3>
-                </div>
-                <div className='content email-section'>Contact me at:
-                    <div className='email'>
-                        <a id='email' href="mailto:thomaschestnut00@gmail.com">thomaschestnut00@gmail.com</a>
-                        <button className='email-btn'><FontAwesomeIcon onClick={copyEmail} icon={faCopy} /></button>
-                    </div>
-                </div>
-                {emailCopied && (
-                    <span className='email-message'>
-                        {emailCopied}
-                    </span>
-                )}
-                <div>
-                    <h3 className='heading'>LinkedIn</h3>
-                </div>
-                <p className='content linkedin-section'>Connect with me on:
-                    <a className='linkedin' href="https://www.linkedin.com/in/thomas-chestnut/" target='_blank' rel='noreferrer'>LinkedIn</a>
-                </p>
-            </div>
-        </section>
-    );
+	return (
+		<section className='contact-me-section'>
+			<h2 className='heading'>Contact Me</h2>
+			<div>
+				<div>
+					<div className='content email-section'>
+						<h3 className='sub-heading'>Email</h3>
+						<p>Contact me at:</p>
+						<div className='email'>
+							<a
+								id='email'
+								href='mailto:thomaschestnut00@gmail.com'
+							>
+								thomaschestnut00@gmail.com
+							</a>
+							<button className='email-btn'>
+								<FontAwesomeIcon
+									onClick={copyEmail}
+									icon={faCopy}
+								/>
+							</button>
+						</div>
+						{emailCopied && (
+							<span
+								style={{ marginTop: '10px' }}
+								className='email-message'
+							>
+								{emailCopied}
+							</span>
+						)}
+					</div>
+				</div>
+				<div>
+					<div className='content linkedin-section'>
+						<h3 className='sub-heading'>LinkedIn</h3>
+						<p>Connect with me on:</p>
+						<a
+							className='linkedin'
+							href='https://www.linkedin.com/in/thomas-chestnut/'
+							target='_blank'
+							rel='noreferrer'
+						>
+							LinkedIn
+						</a>
+					</div>
+				</div>
+			</div>
+		</section>
+	);
 }
 
 export default Contact;
--- src/utils/projects.js
+++ src/utils/projects.js
@@ -2,84 +2,88 @@ import photo1 from '../assets/images/pass-the-page.png';
 import photo2 from '../assets/images/AniGame.png';
 import photo3 from '../assets/images/code-zone.png';
 import photo4 from '../assets/images/social-network.png';
-import photo5 from '../assets/images/team-builder.png';
-import photo6 from '../assets/images/weather-dashboard.png';
 import photo7 from '../assets/images/lite-speed.png';
 import photo8 from '../assets/images/book-search.png';
 
 const projects = [
-    {
-        id: '7',
-        title: 'Lite Speed',
-        url: 'https://lite-speed.herokuapp.com/',
-        repo: 'lite-speed',
-        image: photo7,
-        features: 'Full-Stack MERN App, JavaScript, React, Express.js, Node.js, MongoDB, Mongoose, GraphQL, Apollo Server, JWT',
-        type: 'full-stack'
-    },
-    {
-        id: '1',
-        title: 'Pass the Page',
-        url: 'https://passthepage.herokuapp.com/',
-        repo: 'pass-the-page',
-        image: photo1,
-        features: 'Full Stack, JavaScript, Express.js, Node.js, MySQL, Sequelize, RESTful API, CSS, Bootstrap, Handlebars, MVC',
-        type: 'full-stack'
-    },
-    {
-        id: '2',
-        title: 'AniGame',
-        url: 'https://cfsylvester.github.io/AniGame/',
-        repo: 'AniGame',
-        image: photo2,
-        features: 'JavaScript, jQuery, HTML, CSS, Bulma Framework, GameSpot API, RAWG API, Kitsu API',
-        type: 'front-end'
-    },
-    {
-        id: '3',
-        title: 'Code Zone',
-        url: 'https://code-zone.herokuapp.com/',
-        repo: 'code-zone',
-        image: photo3,
-        features: 'Full Stack, JavaScript, Express.js, Node.js, MySQL, Sequelize, RESTful API, CSS, MVC',
-        type: 'full-stack'
-    },
-    {
-        id: '8',
-        title: 'Book Search',
-        url: 'https://booksearch-tc.herokuapp.com/',
-        repo: 'book-search',
-        image: photo8,
-        features: 'Full-Stack MERN App, JavaScript, React, MongoDB, Express.js, Node.js, GraphQL API, Apollo Server, JWT',
-        type: 'full-stack'
-    },
-    {
-        id: '4',
-        title: 'Social Network Backend',
-        url: 'http://bit.ly/social-tc',
-        repo: 'social-network',
-        image: photo4,
-        features: 'Back-End, JavaScript, Express.js, Node.js, RESTful API, NoSQL Database, MongoDB, Mongoose',
-        type: 'back-end'
-    },
-    // {
-    //     id: '5',
-    //     title: 'Team Builder',
-    //     url: 'http://bit.ly/teambuilder-tc',
-    //     repo: 'team-builder',
-    //     image: photo5,
-    //     features: 'Back-End, JavaScript, Node.js, CSS, Inquirer.js, HTML Generator',
-    //     type: 'back-end'
-    // },
-    // {
-    //     id: '6',
-    //     title: 'Weather Dashboard',
-    //     url: 'http://bit.ly/weatherapp-tc',
-    //     repo: 'weather-dashboard',
-    //     image: photo6,
-    //     features: 'Front-End, JavaScript, CSS, Bootstrap, OpenWeather API, AJAX',
-    //     type: 'front-end'
-    // }
+	{
+		id: '7',
+		title: 'Lite Speed',
+		url: 'https://lite-speed.herokuapp.com/',
+		repo: 'lite-speed',
+		image: photo7,
+		features:
+			'Full-Stack MERN App, JavaScript, React, Express.js, Node.js, MongoDB, Mongoose, GraphQL, Apollo Server, JWT',
+		type: 'full-stack',
+	},
+	{
+		id: '1',
+		title: 'Pass the Page',
+		url: 'https://passthepage.herokuapp.com/',
+		repo: 'pass-the-page',
+		image: photo1,
+		features:
+			'Full Stack, JavaScript, Express.js, Node.js, MySQL, Sequelize, RESTful API, CSS, Bootstrap, Handlebars, MVC',
+		type: 'full-stack',
+	},
+	{
+		id: '2',
+		title: 'AniGame',
+		url: 'https://cfsylvester.github.io/AniGame/',
+		repo: 'AniGame',
+		image: photo2,
+		features:
+			'JavaScript, jQuery, HTML, CSS, Bulma Framework, GameSpot API, RAWG API, Kitsu API',
+		type: 'front-end',
+	},
+	{
+		id: '3',
+		title: 'Code Zone',
+		url: 'https://code-zone.herokuapp.com/',
+		repo: 'code-zone',
+		image: photo3,
+		features:
+			'Full Stack, JavaScript, Express.js, Node.js, MySQL, Sequelize, RESTful API, CSS, MVC',
+		type: 'full-stack',
+	},
+	{
+		id: '8',
+		title: 'Book Search',
+		url: 'https://booksearch-tc.herokuapp.com/',
+		repo: 'book-search',
+		image: photo8,
+		features:
+			'Full-Stack MERN App, JavaScript, React, MongoDB, Express.js, Node.js, GraphQL API, Apollo Server, JWT',
+		type: 'full-stack',
+	},
+	{
+		id: '4',
+		title: 'Social Network Backend',
+		url: 'http://bit.ly/social-tc',
+		repo: 'social-network',
+		image: photo4,
+		features:
+			'Back-End, JavaScript, Express.js, Node.js, RESTful API, NoSQL Database, MongoDB, Mongoose',
+		type: 'back-end',
+	},
+	// {
+	//     id: '5',
+	//     title: 'Team Builder',
+	//     url: 'http://bit.ly/teambuilder-tc',
+	//     repo: 'team-builder',
+	//     image: photo5,
+	//     features: 'Back-End, JavaScript, Node.js, CSS, Inquirer.js, HTML Generator',
+	//     type: 'back-end'
+	// },
+	// {
+	//     id: '6',
+	//     title: 'Weather Dashboard',
+	//     url: 'http://bit.ly/weatherapp-tc',
+	//     repo: 'weather-dashboard',
+	//     image: photo6,
+	//     features: 'Front-End, JavaScript, CSS, Bootstrap, OpenWeather API, AJAX',
+	//     type: 'front-end'
+	// }
 ];
 
 export default projects;
