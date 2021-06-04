3601a45294b73f62882a143127bf6abef66047f1
Tom Chestnut <tchestnut85@gmail.com>
2021-06-03 21:29:03 -0400

    Installed React Router and setup routing for the different page sections.

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,22 +1,57 @@
-import React from 'react';
-import pic from '../../assets/images/tom.jpg';
+import React, { useEffect, useState } from 'react';
 
 function About() {
-    return (
-        <section>
-            <h2 className='heading'>Hello! I'm Tom.</h2>
-            <div className='bio-container'>
-                <img className='my-pic' src={pic} alt='Tom Chestnut holding his pug in front of a stream.' />
-                <p className='bio'>
-                    Full Stack Developer specializing in MERN stack development and skilled in creating responsive web applications. Earned a Full Stack Development Certificate from University of Pennsylvania LPS Coding Bootcamp.
-                    <br />
-                    I have created multiple projects using front-end technology such as JavaScript, React, HTML, and CSS as well as back-end tools like Node.js, Express.js, MongoDB, GraphQL and MySQL while following to development methods such as Agile development, RESTful APIs, MVC structure and Single Page Applications (SPAs).
-                    <br />
-                    When I'm not coding, I spend time going for hikes with my wife and our pug, or I may be playing video games like Skyrim, Legend of Zelda, or Assasin's Creed. I also enjoy traveling when able to, having visited Japan and Italy in the past and hope to see more countries in the future.
-                </p>
-            </div>
-        </section>
-    );
+	const githubEndpoint = 'https://api.github.com/users/tchestnut85';
+	const [githubData, setGithubData] = useState('');
+
+	useEffect(() => {
+		getGithubData();
+	}, []);
+
+	const getGithubData = async () => {
+		try {
+			const res = await fetch(githubEndpoint);
+			const data = await res.json();
+			setGithubData(data);
+		} catch (err) {
+			console.error(err);
+		}
+	};
+
+	return (
+		<section>
+			<h2 className='heading'>Hello! I'm Tom.</h2>
+			<h3 className='sub-heading'>Full Stack JavaScript Developer</h3>
+			<div className='bio-container'>
+				<img
+					className='my-pic'
+					src={githubData.avatar_url}
+					alt='Tom Chestnut holding his pug Frodo in front of a stream.'
+				/>
+
+				<article className='bio'>
+					I'm a Full Stack JavaScript Developer specializing in the
+					MERN stack and skilled in creating responsive web
+					applications. I've earned a Full Stack Development
+					Certificate from University of Pennsylvania LPS Coding
+					Bootcamp.
+					<br />
+					I have created multiple projects using front-end technology
+					such as JavaScript, React, HTML, and CSS as well as back-end
+					tools like Node.js, Express.js, MongoDB, GraphQL and MySQL
+					while following to development methods such as Agile
+					development, RESTful APIs, MVC structure and Single Page
+					Applications (SPAs).
+					<br />
+					When I'm not coding, I spend time going for hikes with my
+					wife and our pug, or I may be playing video games like
+					Skyrim, Legend of Zelda, or Assasin's Creed. I also enjoy
+					traveling when able to, having visited Japan and Italy in
+					the past and hope to see more countries in the future.
+				</article>
+			</div>
+		</section>
+	);
 }
 
 export default About;
--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,31 +1,45 @@
+import { Link } from 'react-router-dom';
 import React from 'react';
 
 function Nav(props) {
+	const { sections, currentSection, setCurrentSection } = props;
 
-    const {
-        sections,
-        currentSection,
-        setCurrentSection
-    } = props;
-
-    return (
-        <nav className='navbar'>
-            <h1 className='my-name'>
-                Tom Chestnut
-            </h1>
-            <ul className='flex space-a'>
-                {sections.map((section) => (
-                    <li key={section.name}
-                        className={currentSection === section.name ? 'active list' : 'list'}>
-                        <span
-                            className={currentSection === section.name ? '' : 'list-hover'}
-                            onClick={() => setCurrentSection(section.name)}
-                        >{section.name}</span>
-                    </li>
-                ))}
-            </ul>
-        </nav>
-    );
-};
+	return (
+		<nav className='navbar'>
+			<h1 className='my-name'>Tom Chestnut</h1>
+			<ul className='flex space-a'>
+				{sections.map(section => (
+					<li
+						key={section.name}
+						className={
+							currentSection === section.name
+								? 'active list'
+								: 'list'
+						}
+					>
+						<span
+							className={
+								currentSection === section.name
+									? ''
+									: 'list-hover'
+							}
+							onClick={() => setCurrentSection(section.name)}
+						>
+							<Link
+								to={
+									section.name === 'About'
+										? '/'
+										: `/${section.name.toLowerCase()}`
+								}
+							>
+								{section.name}
+							</Link>
+						</span>
+					</li>
+				))}
+			</ul>
+		</nav>
+	);
+}
 
 export default Nav;
