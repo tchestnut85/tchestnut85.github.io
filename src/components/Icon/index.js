b81efd7b4eb0a386809232ab818f29c3eec34000
Tom Chestnut <tchestnut85@gmail.com>
2021-06-27 13:06:18 -0400

    Updated CSS for the navbar items. Made a utils file for the footer links to import into the footer component.

--- src/components/Icon/index.js
+++ src/components/Icon/index.js
@@ -1,42 +1,50 @@
-import React from 'react';
+import {
+	faGithubSquare,
+	faHackerrank,
+	faLinkedin,
+	faStackOverflow,
+} from '@fortawesome/free-brands-svg-icons';
+
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faLinkedin, faGithubSquare, faHackerrank, faStackOverflow } from '@fortawesome/free-brands-svg-icons';
+import React from 'react';
 import { faLaptop } from '@fortawesome/free-solid-svg-icons';
 
-function Icon(props) {
+function Icon({ links }) {
+	function iconName(id) {
+		switch (id) {
+			case 'link-1':
+				return faLinkedin;
+			case 'link-2':
+				return faGithubSquare;
+			case 'link-3':
+				return faHackerrank;
+			case 'link-4':
+				return faStackOverflow;
+			default:
+				return faLaptop;
+		}
+	}
 
-    const { links } = props;
-
-    function iconName(id) {
-        switch (id) {
-            case 'link-1':
-                return faLinkedin;
-            case 'link-2':
-                return faGithubSquare;
-            case 'link-3':
-                return faHackerrank;
-            case 'link-4':
-                return faStackOverflow;
-            default:
-                return faLaptop;
-        }
-    }
-
-    return (
-        <div className='flex centered wrap'>
-            {links.map(link => (
-                <span key={(`${link.icon}-span`)}>
-                    <a href={link.url} target='_blank' rel='noreferrer' className='icon-link link'>
-                        <FontAwesomeIcon
-                            className='icons'
-                            key={(`${link.icon}-icon`)}
-                            icon={iconName(link.id)}
-                        />
-                    </a>
-                </span>
-            ))}
-        </div>
-    );
+	return (
+		<div className='flex centered wrap'>
+			{links.map(link => (
+				<span key={`${link.icon}-span`}>
+					<a
+						href={link.url}
+						target='_blank'
+						rel='noreferrer'
+						className='icon-link link'
+					>
+						<FontAwesomeIcon
+							className='icons'
+							key={`${link.icon}-icon`}
+							icon={iconName(link.id)}
+						/>
+					</a>
+				</span>
+			))}
+		</div>
+	);
 }
 
 export default Icon;
---
+++ src/utils/footerLinks.js
@@ -0,0 +1,26 @@
+export const links = [
+	{
+		id: 'link-1',
+		name: 'LinkedIn',
+		url: 'https://www.linkedin.com/in/thomas-chestnut/',
+		icon: 'linkedin',
+	},
+	{
+		id: 'link-2',
+		name: 'GitHub',
+		url: 'https://github.com/tchestnut85',
+		icon: 'github-square',
+	},
+	{
+		id: 'link-3',
+		name: 'HackerRank',
+		url: 'https://www.hackerrank.com/tchestnut85',
+		icon: 'hackerrank',
+	},
+	{
+		id: 'link-4',
+		name: 'Stack Overflow',
+		url: 'https://stackoverflow.com/users/13815310/tchestnut85',
+		icon: 'stack-overflow',
+	},
+];
