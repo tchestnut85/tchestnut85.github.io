ac07e7abc208df5ea94f0855c2c460ca68a16793
Tom Chestnut <tchestnut85@gmail.com>
2022-09-29 14:37:29 -0400

    update resume section

--- src/constants/skills.js
+++ src/constants/skills.js
@@ -1,117 +1,121 @@
+export const RESUME_URL =
+	'https://drive.google.com/file/d/1M4PNYrAi1U5XrBmbfFSP-DXe4pLXNSMs/view?usp=sharing';
+
+export const RESUME_TEXT = {
+	text: 'View or download my full resume ',
+	here: 'here.',
+};
+
+export const SKILL_TYPES = {
+	front: { id: 'front_end', label: 'Front-End Skills' },
+	back: { id: 'back_end', label: 'Back-End Skills' },
+	other: { id: 'other', label: 'Other Skills' },
+};
+
 export const SKILLS = [
 	{
 		name: 'JavaScript',
 		file: 'javascript.svg',
-		type: 'front-end',
+		type: SKILL_TYPES.front.id,
 	},
 	{
 		name: 'React',
 		file: 'react.svg',
-		type: 'front-end',
+		type: SKILL_TYPES.front.id,
 	},
 	{
 		name: 'Redux',
 		file: 'redux.svg',
-		type: 'front-end',
+		type: SKILL_TYPES.front.id,
 	},
 	{
 		name: 'HTML',
 		file: 'html-5.svg',
-		type: 'front-end',
+		type: SKILL_TYPES.front.id,
 	},
 	{
 		name: 'CSS',
 		file: 'css.svg',
-		type: 'front-end',
+		type: SKILL_TYPES.front.id,
 	},
 	{
 		name: 'Sass',
 		file: 'sass.svg',
-		type: 'front-end',
+		type: SKILL_TYPES.front.id,
 	},
-	{
-		name: 'CSSmodules',
-		file: 'cssmodules.svg',
-		type: 'front-end',
-	},
-	// {
-	// 	name: 'jQuery',
-	// 	file: 'jquery.svg',
-	// 	type: 'front-end',
-	// },
 	{
 		name: 'Node.js',
 		file: 'nodejs.svg',
-		type: 'back-end',
+		type: SKILL_TYPES.back.id,
 	},
 	{
 		name: 'Express.js',
 		file: 'expressjs.svg',
-		type: 'back-end',
+		type: SKILL_TYPES.back.id,
 	},
 	{
 		name: 'Bootstrap',
 		file: 'bootstrap.svg',
-		type: 'front-end',
+		type: SKILL_TYPES.front.id,
 	},
 	{
 		name: 'Materialize',
 		file: 'materializecss.svg',
-		type: 'front-end',
+		type: SKILL_TYPES.front.id,
 	},
 	{
 		name: 'Bulma',
 		file: 'bulma.svg',
-		type: 'front-end',
+		type: SKILL_TYPES.front.id,
 	},
 	{
 		name: 'Jest',
 		file: 'jest.svg',
-		type: 'other',
+		type: SKILL_TYPES.other.id,
 	},
 	{
 		name: 'MongoDB',
 		file: 'mongodb.svg',
-		type: 'back-end',
+		type: SKILL_TYPES.back.id,
 	},
 	{
 		name: 'Mongoose',
 		file: 'mongoose.png',
-		type: 'back-end',
+		type: SKILL_TYPES.back.id,
 	},
 	{
 		name: 'GraphQL | Apollo Client',
 		file: 'graphql.svg',
-		type: 'front-end',
+		type: SKILL_TYPES.front.id,
 	},
 	{
 		name: 'GraphQL | Apollo Server',
 		file: 'apollo.svg',
-		type: 'back-end',
+		type: SKILL_TYPES.back.id,
 	},
 	{
 		name: 'MySQL',
 		file: 'mysql.svg',
-		type: 'back-end',
+		type: SKILL_TYPES.back.id,
 	},
 	{
 		name: 'Sequelize',
 		file: 'sequelize.svg',
-		type: 'back-end',
+		type: SKILL_TYPES.back.id,
 	},
 	{
 		name: 'Git',
 		file: 'git-icon.svg',
-		type: 'other',
+		type: SKILL_TYPES.other.id,
 	},
 	// {
 	// 	name: 'Webpack',
 	// 	file: 'webpack.svg',
-	// 	type: 'front-end',
+	// 	type: SKILL_TYPES.front.id,
 	// },
 	{
 		name: 'Heroku',
 		file: 'heroku.svg',
-		type: 'other',
+		type: SKILL_TYPES.other.id,
 	},
 ];
