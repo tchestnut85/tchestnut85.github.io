7df4c506f9aa0183710b1ddae01be7df5a014891
Tom Chestnut <tchestnut85@gmail.com>
2026-06-08 16:26:05 -0400

    fix some package warnings, update gh-pages, fix resume section

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -1,14 +1,10 @@
 import React from 'react';
 
-import {
-	SKILLS,
-	SKILL_TYPES,
-	RESUME_TEXT,
-	RESUME_URL,
-} from '../../constants/skills';
+import { SKILLS, SKILL_TYPES, RESUME_TEXT, RESUME_URL } from '../../constants/skills';
 
 const SKILL_TYPE_KEYS = Object.keys(SKILL_TYPES);
 
+// eslint-disable-next-line no-unused-vars
 const skills = SKILL_TYPE_KEYS.reduce((acc, key) => {
 	const skillId = SKILL_TYPES[key].id;
 	const newSkillObj = {
@@ -18,6 +14,7 @@ const skills = SKILL_TYPE_KEYS.reduce((acc, key) => {
 	return [...acc, newSkillObj];
 }, []);
 
+// eslint-disable-next-line no-unused-vars
 const ResumeSection = ({ skills }) => {
 	return (
 		<>
@@ -36,34 +33,25 @@ const ResumeSection = ({ skills }) => {
 };
 
 function Resume() {
-	const renderSkillSections = () => {
-		return skills.map(({ heading, list }) => (
-			<div key={heading}>
-				<h3 className="heading">{heading}</h3>
-				<div className="icon-list">
-					<ResumeSection skills={list} />
-				</div>
-			</div>
-		));
-	};
-
 	return (
 		<section className="resume-section">
 			<div>
 				<h2 className="heading">Resume</h2>
 			</div>
-			<p className="content resume-text">
+			<p className="content resume-content">
 				{RESUME_TEXT.text}
-				<a
-					className="resume-link"
-					href={RESUME_URL}
-					target="_blank"
-					rel="noopener noreferrer"
-				>
+				<a className="resume-link" href={RESUME_URL} target="_blank" rel="noopener noreferrer">
 					{RESUME_TEXT.here}
 				</a>
 			</p>
-			{renderSkillSections()}
+			{/* {skills.map(({ heading, list }) => (
+				<div key={heading}>
+					<h3 className="heading">{heading}</h3>
+					<div className="icon-list">
+						<ResumeSection skills={list} />
+					</div>
+				</div>
+			))} */}
 		</section>
 	);
 }
